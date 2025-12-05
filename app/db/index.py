915db79b09afa36cd4510df7c8425043a37220f4
edson costa <edsonm737@gmail.com>
2025-12-05 14:53:46 -0300

    inicio da implemntação da pagina index

--- app/db/index.py
+++ app/db/index.py
@@ -5,7 +5,10 @@ def perfil_modelos():
 
     return[
 
-        {"nome": "Modelo 1", "descricao": "Descrição do modelo 1", "imagem": "modelo1.jpg"},
-        {"nome": "Modelo 2", "descricao": "Descrição do modelo 2", "imagem": "modelo2.jpg"},
-        {"nome": "Modelo 3", "descricao": "Descrição do modelo 3", "imagem": "modelo3.jpg"},
+        {"nome": "Niemira", "descricao": "Descrição do modelo 1", "imagem": "../../../static/img/destaques/Niemira_1.jpg"},
+        {"nome": "VickiWade", "descricao": "Descrição do modelo 2", "imagem": "../../../static/img/destaques/VickiWade_1.jpg"},
+        {"nome": "Wanda", "Wanda": "Descrição do modelo 3", "imagem": "../../../static/img/destaques/Wanda_1.jpg"},
+        {"nome": "Alissa Foxy", "descricao": "Descrição do modelo 1", "imagem": "../../../static/img/destaques/AlissaFoxy_1.jpg"},
+        {"nome": "AriaA lexander", "descricao": "Descrição do modelo 2", "imagem": "../../../static/img/destaques/AriaAlexander_1.jpg"},
+        {"nome": "Jillian Janson", "descricao": "Descrição do modelo 3", "imagem": "../../../static/img/destaques/JillianJanson_1.jpg"},
     ]
